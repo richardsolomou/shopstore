4e6a1b0cface077bd04f898fedc66619e6698240
Richard Solomou <rsolomou@gmail.com>
2013-03-29 21:12:09 +0000

    Added a URL router.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+	/**
+	 * Start the session.
+	 */
+	session_start();
+
+	/**
+	 * Define the basic global variables.
+	 */
+	define('DS', DIRECTORY_SEPARATOR);
+	define('SERVER_ROOT', dirname(__FILE__));
+	define('BASE_PATH', dirname($_SERVER['PHP_SELF']) == '/' ? '' : dirname($_SERVER['PHP_SELF']));
+	define('ADMIN_PATH', BASE_PATH . DS . 'admin' . DS);
+
+	/**
+	 * Write the raw URL to a variable to be used in a later function.
+	 */
+	$url = $_GET['url'];
+
+	/**
+	 * Include all element classes.
+	 */
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'categories.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'currencies.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'customers.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'managers.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'products.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'reviews.php';
+	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'settings.php';
+
+	/**
+	 * Include the server configuration and core functions.
+	 */
+	require_once SERVER_ROOT . DS . 'library' . DS . 'config.php';
+	require_once SERVER_ROOT . DS . 'library' . DS . 'core.php';
+
+?>
---
+++ library/core.php
@@ -0,0 +1,52 @@
+<?php
+
+	function setReporting()
+	{
+		if (DEVELOPMENT_ENVIRONMENT == true) {
+			error_reporting(E_ALL);
+			ini_set('display_errors', '1');
+		} else {
+			error_reporting(E_ALL);
+			ini_set('display_errors', '0');
+			ini_set('log_errors', '1');
+			ini_set('error_log', SERVER_ROOT . 'error.log');
+		}
+	}
+
+	function setRouting()
+	{
+		global $url;
+
+		if (isset($url)) {
+			$urlArray = array();
+			$urlArray = explode('/', $url);
+			$controller = $urlArray[0];
+			array_shift($urlArray);
+			if (isset($urlArray[0])) {
+				$action = $urlArray[0];
+				array_shift($urlArray);
+			} else {
+				$action = '';
+			}
+			$queryString = $urlArray;
+		} else {
+			$controller = '';
+			$action = '';
+		}
+		if (file_exists(SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . $controller . '.php')) {
+			$controllerName = ucfirst($controller);
+			$dispatch = new $controllerName();
+			if (method_exists($controllerName, $action)) {
+				call_user_func_array(array($dispatch, $action), $queryString);
+			}
+		} else {
+			$controllerName = '';
+			$controller = '';
+			$action = '';
+		}
+	}
+
+	setReporting();
+	setRouting();
+
+?>
