15ca273be87d8570d0bafb68345a299a4f98c546
Richard Solomou <rsolomou@gmail.com>
2013-03-30 02:48:19 +0000

    Broke up functions for readability and added comments.

--- library/core.php
+++ library/core.php
@@ -1,5 +1,9 @@
 <?php
 
+	/**
+	 * If currently in development display errors, otherwise hide and log
+	 * the errors in a file.
+	 */
 	function setReporting()
 	{
 		if (DEVELOPMENT_ENVIRONMENT == true) {
@@ -13,9 +17,38 @@
 		}
 	}
 
+	/**
+	 * Checks if the element controller file exists and moves on to check for
+	 * the method given. If either one doesn't exist, it returns a 404 error.
+	 */
+	function setControllerView($controller, $action, $queryString)
+	{
+		if (file_exists(SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . $controller . '.php')) {
+			$controllerName = ucfirst($controller);
+			$dispatch = new $controllerName;
+			$action = empty($action) ? 'defaultPage' : $action;
+			if (method_exists($controllerName, $action)) {
+				call_user_func_array(array($dispatch, $action), $queryString);
+			} else {
+				header("HTTP/1.0 404 Not Found");
+				require_once SERVER_ROOT . DS . 'application' . DS . 'views' . DS . '404.php';
+			}
+		} else {
+			header("HTTP/1.0 404 Not Found");
+			require_once SERVER_ROOT . DS . 'application' . DS . 'views' . DS . '404.php';
+		}
+	}
+
+	/**
+	 * Break down the URL in parts to figure out the controller and action,
+	 * and then create a new instance of that controller.
+	 */
 	function setRouting()
 	{
+		// Use the $url variable from /index.php
 		global $url;
+
+		$controller = $action = $controllerName = null;
 
 		if (isset($url)) {
 			$urlArray = array();
@@ -25,27 +58,17 @@
 			if (isset($urlArray[0])) {
 				$action = $urlArray[0];
 				array_shift($urlArray);
-			} else {
-				$action = '';
 			}
 			$queryString = $urlArray;
+			setControllerView($controller, $action, $queryString);
 		} else {
-			$controller = '';
-			$action = '';
-		}
-		if (file_exists(SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . $controller . '.php')) {
-			$controllerName = ucfirst($controller);
-			$dispatch = new $controllerName();
-			if (method_exists($controllerName, $action)) {
-				call_user_func_array(array($dispatch, $action), $queryString);
-			}
-		} else {
-			$controllerName = '';
-			$controller = '';
-			$action = '';
+			// If the URL is empty, render the default index page.
+			$dispatch = new Controller;
+			call_user_func_array(array($dispatch, 'defaultPage'), array());
 		}
 	}
 
+	// Execute the two functions
 	setReporting();
 	setRouting();
 
