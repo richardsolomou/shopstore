9ca496bfcefa269091c9ae9a42f858c68b1dedb2
Richard Solomou <rsolomou@gmail.com>
2013-03-31 05:41:26 +0100

    Created the Category model class.

---
+++ application/models/category.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 * Extends the Model category in order to serve as the main element model
+	 * for the Categories controller class.
+	 */
+	class Category extends Model
+	{
+		
+	}
+
+?>
