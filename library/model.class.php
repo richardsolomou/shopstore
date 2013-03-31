239efa50d908481c8adc956616a1722c7ede7879
Richard Solomou <rsolomou@gmail.com>
2013-03-31 05:20:00 +0100

    Created the basic model class.

---
+++ library/model.class.php
@@ -0,0 +1,43 @@
+<?php
+
+	/**
+	 * Extends the database and is extended by individual element class models.
+	 */
+	class Model extends Database
+	{
+
+		/**
+		 * Contains the name of the model currently being used.
+		 * @var string
+		 * @access protected
+		 */
+		protected $_model;
+
+		/**
+		 * Activates a connection to the database using the global variables in
+		 * the configuration file and assigns the default table according to
+		 * the model class being used at the moment.
+		 *
+		 * @access public
+		 */
+		public function __construct()
+		{
+			global $inflect;
+			$this->connect(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+			$this->_model = get_class($this);
+			$this->table($inflect->pluralize(strtolower($this->_model)));
+		}
+
+		/**
+		 * Closes the connection to the database.
+		 *
+		 * @access public
+		 */
+		public function __destruct()
+		{
+			$this->disconnect();
+		}
+
+	}
+
+?>
