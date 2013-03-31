873200831233517c3ee947ff4c892b91724e5041
Richard Solomou <rsolomou@gmail.com>
2013-03-31 18:10:34 +0100

    Moved class inclusions to a standalone file.

--- index.php
+++ index.php
@@ -19,33 +19,9 @@
 	$url = isset($_GET['url']) ? $_GET['url'] : null;
 
 	/**
-	 * Include the inflection class.
+	 * Include the global classes file.
 	 */
-	require_once SERVER_ROOT . DS . 'library' . DS . 'inflect.class.php';
-	$inflect = new Inflect;
-
-	/**
-	 * Include the main controller and the database.
-	 */
-	require_once SERVER_ROOT . DS . 'library' . DS . 'database.class.php';
-	require_once SERVER_ROOT . DS . 'library' . DS . 'model.class.php';
-	require_once SERVER_ROOT . DS . 'library' . DS . 'controller.class.php';
-	
-	/**
-	 * Include all element class controllers.
-	 */
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'categories.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'currencies.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'customers.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'managers.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'products.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'reviews.php';
-	require_once SERVER_ROOT . DS . 'application' . DS . 'controllers' . DS . 'settings.php';
-
-	/**
-	 * Include the element class models.
-	 */
-	require_once SERVER_ROOT . DS . 'application' . DS . 'models' . DS . 'category.php';
+	require_once SERVER_ROOT . DS . 'library' . DS . 'classes.php';
 
 	/**
 	 * Include the server configuration and core functions.
