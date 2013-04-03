596c0805e5cead2bae46eaf9a137a7040eab3749
Richard Solomou <rsolomou@gmail.com>
2013-04-03 18:16:53 +0100

    Added the files consisting of the template of the website.

---
+++ application/views/installer/error.php
@@ -0,0 +1,11 @@
+<article id="full">
+
+    <h1><?php echo $pageTitle ?></h1>
+
+    <?php foreach ($error as $err) { ?>
+    	<div class="alert"><?php echo $err; ?></div>
+	<?php } ?>
+
+    <a href="<?php echo BASE_PATH . DS . 'installer'; ?>" class="btn">Back to Installer</a>
+
+</article>
---
+++ application/views/installer/second.php
@@ -0,0 +1,7 @@
+<article id="full">
+
+	<p class="centered">Setup has successfully finished.</p>
+	<p>&nbsp;</p>
+	<p class="centered"><a href="<?php echo BASE_PATH . DS . 'admin'; ?>" class="btn">Continue To Administration Area</a></p>
+
+</article>
