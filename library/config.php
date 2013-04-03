e48c715c7f14a99bf9654f0a468818070dc7ce77
Richard Solomou <rsolomou@gmail.com>
2013-04-03 18:53:03 +0100

    Readded the configuration file.

---
+++ library/config.php
@@ -0,0 +1,21 @@
+<?php
+
+	/**
+	 * Set the under-development environment variable.
+	 */
+	define ('DEVELOPMENT_ENVIRONMENT', true);
+
+	/**
+	 * Set configuration variables.
+	 */
+	define('DB_HOST', '127.0.0.1');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'webscrp');
+	
+	/**
+	 * Automatically set by the installer.
+	 */
+	define('DB_SETUP', false);
+
+?>
