bbe4cfb9659bd4dc50c6cdbbe3378a351287211e
Richard Solomou <rsolomou@gmail.com>
2013-04-03 01:30:40 +0100

    Prepared automatic installer model class.

---
+++ application/models/installer.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 * Extends the Model class in order to serve as the main element model
+	 * for the Installer controller class.
+	 */
+	class Installer extends Model
+	{
+		
+	}
+
+?>
