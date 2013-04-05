ec275d0c48fb70b4d2413e150349fbe7555cd05b
Richard Solomou <rsolomou@gmail.com>
2013-04-05 05:40:53 +0100

    Deleted unused files.

--- application/views/categories/error.php
+++
@@ -1,4 +0,0 @@
-<article>
-	<h1>Category not found</h1>
-	<p>The category you're looking for was not found. Please check the URL and try again.</p>
-</article>
--- application/views/products/error.php
+++
@@ -1,4 +0,0 @@
-<article>
-	<h1>Product not found</h1>
-	<p>The product you're looking for was not found. Please check the URL and try again.</p>
-</article>
