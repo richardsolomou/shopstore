3d9898050218190f62a27b28316f8b28821ddd40
Richard Solomou <rsolomou@gmail.com>
2013-04-06 19:47:35 +0100

    Fixed horizontal rule margin and added a function to get the form values in a form in JavaScript.

--- templates/js/layercms.js
+++ templates/js/layercms.js
@@ -17,6 +17,7 @@ layercms.webscrp = (function() {
 		toggleHide,
 		toggleShowHide,
 		loader,
+		getFormValue,
 		doAdd,
 		doEdit,
 		getAddForm,
@@ -89,34 +90,42 @@ layercms.webscrp = (function() {
 
 	};
 
+	getFormValue = function (form, name) {
+		if (form && form[name] && form[name].value) return "&" + name + "=" + form[name].value;
+		return "";
+	};
+
 	// Creates and assigns the variables required to be sent to the loader function and gets all
 	// the input fields from the form page and assigns them to variables to be sent as parameters.
-	doAdd = function (obj) {
+	doAdd = function (obj, formID) {
 
 		var method = 'POST';
 		var url = 'insert/';
 		var params;
 		var target = 'operationAlert';
+
+		var form = document.getElementById(formID);
 		
 		switch (obj) {
 			case 'administrators':
-				params = getAdministratorParams();
 				break;
 			case 'categories':
-				params = getCategoryParams();
+				params = getFormValue(form, 'category_name') + getFormValue(form, 'category_parent_ID');
 				break;
 			case 'currencies':
-				params = getCurrencyParams();
+				params = getFormValue(form, 'currency_name') + getFormValue(form, 'currency_code') + getFormValue(form, 'currency_symbol');
 				break;
 			case 'customers':
-				params = getCustomerParams();
+				params = getFormValue(form, 'customer_username') + getFormValue(form, 'customer_password') + getFormValue(form, 'customer_firstname') + getFormValue(form, 'customer_lastname') + getFormValue(form, 'customer_address1') + getFormValue(form, 'customer_address2') + getFormValue(form, 'customer_postcode') + getFormValue(form, 'customer_phone') + getFormValue(form, 'customer_email');
 				break;
 			case 'products':
-				params = getProductParams();
+				params = getFormValue(form, 'category_ID') + getFormValue(form, 'product_name') + getFormValue(form, 'product_description') + getFormValue(form, 'product_condition') + getFormValue(form, 'product_price') + getFormValue(form, 'product_stock') + getFormValue(form, 'product_image');
 				break;
 			case 'reviews':
-				params = getReviewParams();
+				params = getFormValue(form, 'product_ID') + getFormValue(form, 'review_subject') + getFormValue(form, 'review_description') + getFormValue(form, 'review_rating') + getFormValue(form, 'customer_ID')
 				break;
+			case 'settings':
+				params = getFormValue(form, 'setting_column') + getFormValue(form, 'setting_value');
 			default:
 				params = '';
 		}
@@ -131,40 +140,36 @@ layercms.webscrp = (function() {
 
 	// Creates and assigns the variables required to be sent to the loader function and gets all
 	// the input fields from the form page and assigns them to variables to be sent as parameters.
-	doEdit = function (obj, id) {
+	doEdit = function (obj, id, formID) {
 
 		var method = 'POST';
 		var url = 'update/' + id + '/';
 		var params;
 		var target = 'operationAlert';
 		
+		var form = document.getElementById(formID);
+
 		switch (obj) {
 			case 'administrators':
-				var admin_ID = document.getElementById('admin_ID').value;
-				params = '&admin_ID=' + admin_ID + getAdministratorParams();
+				params = getFormValue(form, 'admin_ID') + getFormValue(form, 'admin_username') + getFormValue(form, 'admin_password') + getFormValue(form, 'admin_firstname') + getFormValue(form, 'admin_lastname') + getFormValue(form, 'admin_email');
 				break;
 			case 'categories':
-				var category_ID = document.getElementById('category_ID').value;
-				params = '&category_ID=' + category_ID + getCategoryParams();
+				params = getFormValue(form, 'category_ID') + getFormValue(form, 'category_name') + getFormValue(form, 'category_parent_ID');
 				break;
 			case 'currencies':
-				var currency_ID = document.getElementById('currency_ID').value;
-				params = '&currency_ID=' + currency_ID + getCurrencyParams();
+				params = getFormValue(form, 'currency_ID') + getFormValue(form, 'currency_name') + getFormValue(form, 'currency_code') + getFormValue(form, 'currency_symbol');
 				break;
 			case 'customers':
-				var customer_ID = document.getElementById('customer_ID').value;
-				params = '&customer_ID=' + customer_ID + getCustomerParams();
+				params = getFormValue(form, 'customer_ID') + getFormValue(form, 'customer_username') + getFormValue(form, 'customer_password') + getFormValue(form, 'customer_firstname') + getFormValue(form, 'customer_lastname') + getFormValue(form, 'customer_address1') + getFormValue(form, 'customer_address2') + getFormValue(form, 'customer_postcode') + getFormValue(form, 'customer_phone') + getFormValue(form, 'customer_email');
 				break;
 			case 'products':
-				var product_ID = document.getElementById('product_ID').value;
-				params = '&product_ID=' + product_ID + getProductParams();
+				params = getFormValue(form, 'product_ID') + getFormValue(form, 'category_ID') + getFormValue(form, 'product_name') + getFormValue(form, 'product_description') + getFormValue(form, 'product_condition') + getFormValue(form, 'product_price') + getFormValue(form, 'product_stock') + getFormValue(form, 'product_image');
 				break;
 			case 'reviews':
-				var review_ID = document.getElementById('review_ID').value;
-				params = '&review_ID=' + review_ID + getReviewParams();
+				params = getFormValue(form, 'review_ID') + getFormValue(form, 'product_ID') + getFormValue(form, 'review_subject') + getFormValue(form, 'review_description') + getFormValue(form, 'review_rating') + getFormValue(form, 'customer_ID')
 				break;
 			case 'settings':
-				params = getStoreParams();
+				params = getFormValue(form, 'setting_ID') + getFormValue(form, 'setting_column') + getFormValue(form, 'setting_value');
 			default:
 				params = '';
 		}
@@ -209,83 +214,6 @@ layercms.webscrp = (function() {
 			loader(method, url, params, target);
 		}
 
-	};
-
-	// Gets the values of the input fields from the administrator add/edit page
-	// and then returns them to the main function.
-	getAdministratorParams = function () {
-		var admin_username = document.getElementById('admin_username').value;
-		var admin_password = document.getElementById('admin_password').value;
-		var admin_firstname = document.getElementById('admin_firstname').value;
-		var admin_lastname = document.getElementById('admin_lastname').value;
-		var admin_email = document.getElementById('admin_email').value;
-		return '&admin_username=' + admin_username + '&admin_password=' + admin_password + '&admin_firstname=' + admin_firstname + '&admin_lastname=' + admin_lastname + '&admin_email=' + admin_email;
-	};
-
-	// Gets the values of the input fields from the category add/edit page
-	// and then returns them to the main function.
-	getCategoryParams = function () {
-		var category_name = document.getElementById('category_name').value;
-		var category_parent_ID = document.getElementById('category_parent_ID').value;
-		return '&category_name=' + category_name + '&category_parent_ID=' + category_parent_ID;
-	};
-
-	// Gets the values of the input fields from the currency add/edit page
-	// and then returns them to the main function.
-	getCurrencyParams = function () {
-		var currency_name = document.getElementById('currency_name').value;
-		var currency_code = document.getElementById('currency_code').value;
-		var currency_symbol = document.getElementById('currency_symbol').value;
-		return '&currency_name=' + currency_name + '&currency_code=' + currency_code + '&currency_symbol=' + currency_symbol;
-	};
-
-	// Gets the values of the input fields from the customer add/edit page
-	// and then returns them to the main function.
-	getCustomerParams = function () {
-		var customer_username = document.getElementById('customer_username').value;
-		var customer_password = document.getElementById('customer_password').value;
-		var customer_firstname = document.getElementById('customer_firstname').value;
-		var customer_lastname = document.getElementById('customer_lastname').value;
-		var customer_address1 = document.getElementById('customer_address1').value;
-		var customer_address2 = document.getElementById('customer_address2').value;
-		var customer_postcode = document.getElementById('customer_postcode').value;
-		var customer_phone = document.getElementById('customer_phone').value;
-		var customer_email = document.getElementById('customer_email').value;
-		return '&customer_username=' + customer_username + '&customer_password=' + customer_password + '&customer_firstname=' + customer_firstname + '&customer_lastname=' + customer_lastname + '&customer_address1=' + customer_address1 + '&customer_address2=' + customer_address2 + '&customer_postcode=' + customer_postcode + '&customer_phone=' + customer_phone + '&customer_email=' + customer_email;
-	};
-
-	// Gets the values of the input fields from the product add/edit page
-	// and then returns them to the main function.
-	getProductParams = function () {
-		var category_ID = document.getElementById('category_ID').value;
-		var product_name = document.getElementById('product_name').value;
-		var product_description = document.getElementById('product_description').value;
-		var product_condition = document.getElementById('product_condition').value;
-		var product_price = document.getElementById('product_price').value;
-		var product_stock = document.getElementById('product_stock').value;
-		var product_image = document.getElementById('product_image').value.replace('C:\\fakepath\\', '');
-		return '&category_ID=' + category_ID + '&product_name=' + product_name + '&product_description=' + product_description + '&product_condition=' + product_condition + '&product_price=' + product_price + '&product_stock=' + product_stock + '&product_image=' + product_image;
-	};
-
-	// Gets the values of the input fields from the review add/edit page
-	// and then returns them to the main function.
-	getReviewParams = function () {
-		var product_ID = document.getElementById('product_ID').value;
-		var review_subject = document.getElementById('review_subject').value;
-		var review_description = document.getElementById('review_description').value;
-		var review_rating = document.getElementById('review_rating').value;
-		var customer_ID = document.getElementById('customer_ID').value;
-		return '&product_ID=' + product_ID + '&review_subject=' + review_subject + '&review_description=' + review_description + '&review_rating=' + review_rating + '&customer_ID=' + customer_ID;
-	};
-
-	// Gets the values of the input fields from the store edit page
-	// and then returns them to the main function.
-	getStoreParams = function () {
-		var store_name = document.getElementById('store_name').value;
-		var store_address = document.getElementById('store_address').value;
-		var store_phone = document.getElementById('store_phone').value;
-		var currency_ID = document.getElementById('currency_ID').value;
-		return '&store_name=' + store_name + '&store_address=' + store_address + '&store_phone=' + store_phone + '&currency_ID=' + currency_ID;
 	};
 
 	// Creates and assigns the variables required to be sent to the loader function in order to
