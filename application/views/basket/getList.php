c88b5003c4823bc0edb0071759af471c3260fb64
Richard Solomou <rsolomou@gmail.com>
2013-04-21 21:09:56 +0100

    Basket now updates and can send through orders.

--- application/views/basket/getList.php
+++ application/views/basket/getList.php
@@ -19,7 +19,9 @@
             <tbody>
                 <?php
                     $totalPrice = 0;
+                    $numberOfItems = 0;
                     foreach($basketItems as $item) {
+                        $numberOfItems += 1;
                         foreach ($products as $product) {
                             if ($item['product_ID'] == $product['product_ID']) {
                                 $productName = $product['product_name'];
@@ -52,12 +54,16 @@
                 </tr>
             </tbody>
         </table>
-        <p class="centered">
-            <form id="finaliseOrder" method="post">
-                <input type="hidden" name="customer_ID" id="customer_ID" value="<?php echo $item['customer_ID']; ?>">
-                <button class="highlight big">Finalise Order</button>
+        <?php if ($numberOfItems != 0) { ?>
+        <div class="centered">
+            <form id="submitOrder" method="post" onsubmit="layercms.webscrp.submitOrder('submitOrder', '<?php echo BASE_PATH . '/orders/insert'; ?>', '<?php echo BASE_PATH . '/basket/getTable/'; ?>'); return false;">
+                <input type="hidden" name="operation" id="operation" value="true">
+                <input type="hidden" name="customer_ID" id="customer_ID" value="<?php echo $_SESSION['SESS_CUSTOMERID']; ?>">
+                <input type="hidden" name="order_total" id="order_total" value="<?php echo $totalPrice; ?>">
+                <input type="submit" class="highlight big" value="Submit Order">
             </form>
-        </p>
+        </div>
+        <?php } ?>
     </div>
 
 </article>
--- application/views/basket/getTable.php
+++ application/views/basket/getTable.php
@@ -10,7 +10,9 @@
     <tbody>
         <?php
             $totalPrice = 0;
+            $numberOfItems = 0;
             foreach($basketItems as $item) {
+                $numberOfItems += 1;
                 foreach ($products as $product) {
                     if ($item['product_ID'] == $product['product_ID']) {
                         $productName = $product['product_name'];
@@ -28,7 +30,7 @@
                             <input type="hidden" name="basket_ID" id="basket_ID" value="<?php echo $item['basket_ID']; ?>">
                             <input type="hidden" name="customer_ID" id="customer_ID" value="<?php echo $item['customer_ID']; ?>">
                             <input type="hidden" name="product_ID" id="product_ID" value="<?php echo $item['product_ID']; ?>">
-                            <input type="text" class="smaller" id="basket_quantity" placeholder="Quantity" name="basket_quantity" onfocusout="layercms.webscrp.doEdit('basket', <?php echo $item['basket_ID']; ?>, 'updateBasket_<?php echo $item['basket_ID']; ?>', 'operationAlert', null, '<?php echo BASE_PATH . '/basket/getTable/' . $item['product_ID']; ?>')" required value="<?php echo $item['basket_quantity']; ?>" pattern="[0-9]+">
+                            <input type="text" class="smaller" id="basket_quantity" placeholder="Quantity" name="basket_quantity" onfocusout="layercms.webscrp.doEdit('basket', <?php echo $item['basket_ID']; ?>, 'updateBasket_<?php echo $item['basket_ID']; ?>', 'operationAlert', null, '<?php echo BASE_PATH . '/basket/getTable/'; ?>')" required value="<?php echo $item['basket_quantity']; ?>" pattern="[0-9]+">
                             <br><span id="productStockLeft_<?php echo $item['basket_ID']; ?>"><?php echo $productStock; ?></span> left
                         </form>
                     </td>
@@ -43,9 +45,13 @@
         </tr>
     </tbody>
 </table>
-<p class="centered">
-    <form id="finaliseOrder" method="post">
-        <input type="hidden" name="customer_ID" id="customer_ID" value="<?php echo $item['customer_ID']; ?>">
-        <button class="highlight big">Finalise Order</button>
+<?php if ($numberOfItems != 0) { ?>
+<div class="centered">
+    <form id="submitOrder" method="post" onsubmit="layercms.webscrp.submitOrder('submitOrder', '<?php echo BASE_PATH . '/orders/insert'; ?>', '<?php echo BASE_PATH . '/basket/getTable/'; ?>'); return false;">
+        <input type="hidden" name="operation" id="operation" value="true">
+        <input type="hidden" name="customer_ID" id="customer_ID" value="<?php echo $_SESSION['SESS_CUSTOMERID']; ?>">
+        <input type="hidden" name="order_total" id="order_total" value="<?php echo $totalPrice; ?>">
+        <input type="submit" class="highlight big" value="Submit Order">
     </form>
-</p>
+</div>
+<?php } ?>
